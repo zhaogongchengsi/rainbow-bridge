a9508b6c95fed3b701ded7e592825e774e6351c1
zhaogongchengsi <zzh1586169624@163.com>
2024-11-01 00:13:20 +0800

    refactor: 优化代码布局和格式
    优化代码布局和格式，将导入语句中的类型引入改为具体的路径引入。

--- src/renderer/src/database/base.ts
+++ src/renderer/src/database/base.ts
@@ -1,12 +1,12 @@
+import type { Identity } from './types/identit'
 import Dexie, { type EntityTable } from 'dexie'
-import { Identity } from './types/identit'
 
 export class RainbowBridgeDatabase extends Dexie {
   identitys!: EntityTable<Identity, 'id'>
   constructor() {
     super('rainbow-bridge-db')
     this.version(1).stores({
-      identitys: '++id, uuid, create_by, name, email, avatar, comment, lastLoginTime'
+      identitys: '++id, uuid, create_by, name, email, avatar, comment, lastLoginTime',
     })
   }
 }
