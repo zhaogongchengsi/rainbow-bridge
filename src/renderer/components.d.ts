7f76cf81dcc975789ffe0095d56c6a91592ada1a
zhaogongchengsi <zzh1586169624@163.com>
2024-11-16 16:43:04 +0800

    refactor: 添加 PrimeVue 进度加载组件和 Skeleton 组件
    为了增强用户界面的交互性和可视化效果，添加了 PrimeVue 进度加载组件和 Skeleton 组件。进度加载组件用于显示任务的加载进度，而 Skeleton 组件则用于在数据加载时展示占位符，提供更好的用户体验。

--- src/renderer/components.d.ts
+++ src/renderer/components.d.ts
@@ -24,6 +24,7 @@ declare module 'vue' {
     ProgressSpinner: typeof import('primevue/progressspinner')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    Skeleton: typeof import('primevue/skeleton')['default']
     SystemClose: typeof import('@renderer/views/system/system-close.vue')['default']
     SystemFullscreen: typeof import('@renderer/views/system/system-fullscreen.vue')['default']
     SystemFullscreenExit: typeof import('@renderer/views/system/system-fullscreen-exit.vue')['default']
--- src/renderer/src/components/ui/ui-buffer-avatar.vue
+++ src/renderer/src/components/ui/ui-buffer-avatar.vue
@@ -4,17 +4,33 @@ import { type BufferFile, bufferToUrl } from '@renderer/utils/ky'
 const props = defineProps<{
   src: BufferFile
   alt?: string
+  imageClass?: string
 }>()
+
+const divRef = useTemplateRef<HTMLDivElement>('divRef')
+
 const isLoading = ref(true)
 const src = computed(() => bufferToUrl(props.src))
-function handleLoad() {
-  isLoading.value = false
-}
+
+watchEffect(async () => {
+  if (!src.value)
+    return
+  const img = new Image()
+  img.src = src.value
+  img.alt = props.alt || ''
+  img.className = props.imageClass || ''
+  img.onload = () => {
+    isLoading.value = false
+    divRef.value?.appendChild(img)
+  }
+  img.onerror = () => {
+    isLoading.value = false
+  }
+})
 </script>
 
 <template>
-  <div class="overflow-hidden rounded-full">
-    <Skeleton shape="circle" />
-    <img class="size-full rounded-full" :src="src" alt="avatar" @load="handleLoad" @error="handleLoad">
+  <div ref="divRef" class="overflow-hidden rounded-full">
+    <Skeleton v-if="isLoading" class="size-full!" shape="circle" />
   </div>
 </template>
