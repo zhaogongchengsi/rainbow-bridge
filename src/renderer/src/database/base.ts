c4415aae777d2310735a5861c0160392c926d754
zhaogongchengsi <zzh1586169624@163.com>
2024-11-24 21:08:16 +0800

    Optimize Dexie store string generation in RainbowBridgeDatabase

--- src/renderer/src/database/base.ts
+++ src/renderer/src/database/base.ts
@@ -4,8 +4,6 @@ import type { Identity } from './identit'
 import type { Message } from './message'
 import type { User } from './user'
 import Dexie from 'dexie'
-import flatMap from 'lodash/flatMap'
-import union from 'lodash/union'
 import { randomUUID } from 'uncrypto'
 
 export class RainbowBridgeDatabase extends Dexie {
@@ -36,19 +34,5 @@ export class RainbowBridgeDatabase extends Dexie {
 
   createUUID() {
     return randomUUID()
-  }
-
-  generateDexieStoreString(searchableKeys: string[], notSearchableKeys: string[] = []): string {
-    const isPrimaryKey = (key: string) => key.startsWith('++') ? key.replace('++', '') : key
-
-    const combinations = flatMap(searchableKeys, (key, index) => {
-      return searchableKeys.slice(index + 1).map((otherKey) => {
-        return `[${isPrimaryKey(key)}+${otherKey}]`
-      })
-    })
-
-    const searchKeys = searchableKeys.map(key => `[${isPrimaryKey(key)}]`)
-
-    return union([...searchableKeys, ...notSearchableKeys, ...searchKeys, ...combinations]).join(', ')
   }
 }
