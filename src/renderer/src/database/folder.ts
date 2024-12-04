fc32a479f6db69e5b3ce4ce82ff80d1e0161bc0a
zhaogongchengsi <zzh1586169624@163.com>
2024-12-04 22:34:48 +0800

    Add FolderSpace interface for folder synchronization

---
+++ src/renderer/src/database/folder.ts
@@ -0,0 +1,20 @@
+import type { ID } from './type'
+
+export interface FolderSpace {
+  /**
+   * 同步的目标客户
+   */
+  user: ID
+  /**
+   * 同步的根目录
+   */
+  root: string
+  /**
+   * 最后一次同步的时间
+   */
+  lastSyncTime: number
+  /**
+   * 同步的文件夹大小
+   */
+  size: number
+}
