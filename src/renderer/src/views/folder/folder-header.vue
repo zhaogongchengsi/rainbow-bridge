e3416fafe1ed17e7cb154de01fddde939e9dfa24
zhaogongchengsi <zzh1586169624@163.com>
2024-12-05 21:42:23 +0800

    Optimize folder header view by adding a button for adding subfolders

--- src/renderer/src/views/folder/folder-header.vue
+++ src/renderer/src/views/folder/folder-header.vue
@@ -3,7 +3,11 @@
 </script>
 
 <template>
-  <div class="h-12 w-full">
-    folder header
+  <div class="h-12 w-full px-2">
+    <div class="size-full flex items-center">
+      <button class="size-8 rounded-md hover:bg-zinc-300 dark:hover:bg-zinc-700">
+        <span class="pi pi-plus" />
+      </button>
+    </div>
   </div>
 </template>
